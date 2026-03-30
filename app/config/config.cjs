3f0e1f34bdc688ea844e50d755aab31da3f4fbd8
UlianaIvanova <uli.a.na.iva.nova@mail.ru>
2026-03-30 19:41:53 +0300

    Final: remove sub-git and force dev mode

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -1,31 +1,16 @@
 require('dotenv').config();
 
-module.exports = {
-  development: {
-    dialect: 'sqlite',
-    storage: './database.sqlite',
-  },
-  production: {
-    dialect: 'postgres',
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
-    host: process.env.DATABASE_HOST,
-  },
-  // Блок с SQLite закомментирован:
-  // test: {
-  //   dialect: 'sqlite',
-  //   storage: './database.test.sqlite',
-  // },
-  // Блок с Postgres раскомментирован:
-  test: {
-    dialect: 'postgres',
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: 5432, // <--- Проверь, чтобы здесь было 5432
-    host: process.env.DATABASE_HOST,
-  },
+const dbConfig = {
+  dialect: 'postgres',
+  database: process.env.DATABASE_NAME || 'postgres',
+  username: process.env.DATABASE_USERNAME || 'postgres',
+  password: process.env.DATABASE_PASSWORD || 'password',
+  port: process.env.DATABASE_PORT || 5432,
+  host: process.env.DATABASE_HOST || 'db',
+};
 
+module.exports = {
+  development: dbConfig,
+  production: dbConfig,
+  test: dbConfig,
 };
