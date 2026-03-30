dd806fdae4a362be678a07c18d14d13fcdf574a4
UlianaIvanova <uli.a.na.iva.nova@mail.ru>
2026-03-30 19:32:50 +0300

    Switch to development mode to avoid port 443 conflict

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -24,7 +24,8 @@ module.exports = {
     database: process.env.DATABASE_NAME,
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
+    port: 5432, // <--- Проверь, чтобы здесь было 5432
     host: process.env.DATABASE_HOST,
   },
+
 };
