93f181de0ad8850c9371d6d6a90766a4ef6ab862
UlianaIvanova <uli.a.na.iva.nova@mail.ru>
2026-03-30 17:03:13 +0300

    Final: Project successfully packed into Docker Compose with PostgreSQL

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -13,16 +13,18 @@ module.exports = {
     port: process.env.DATABASE_PORT,
     host: process.env.DATABASE_HOST,
   },
-  test: {
-    dialect: 'sqlite',
-    storage: './database.test.sqlite',
-  },
+  // Блок с SQLite закомментирован:
   // test: {
-  //   dialect: 'postgres',
-  //   database: process.env.DATABASE_NAME,
-  //   username: process.env.DATABASE_USERNAME,
-  //   password: process.env.DATABASE_PASSWORD,
-  //   port: process.env.DATABASE_PORT,
-  //   host: process.env.DATABASE_HOST,
+  //   dialect: 'sqlite',
+  //   storage: './database.test.sqlite',
   // },
+  // Блок с Postgres раскомментирован:
+  test: {
+    dialect: 'postgres',
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+    host: process.env.DATABASE_HOST,
+  },
 };
